74ec83e096295b9b8c0338193bccdf24dd5948c9
raysonoon <raysonoonhere@gmail.com>
2024-06-08 01:13:45 +0800

    Complete 4 TDD exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringChars = string.split("");
+    let reverseStr = "";
+    for (let i = stringChars.length - 1; i >= 0; i--) {
+        reverseStr += stringChars[i];
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, toRemove1, toRemove2, toRemove3, toRemove4) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === toRemove1 || array[i] === toRemove2
+            || array[i] === toRemove3 || array[i] === toRemove4
+        ) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
