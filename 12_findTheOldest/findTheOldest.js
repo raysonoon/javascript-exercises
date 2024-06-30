ee8a9fc67e0dcc3ebfc12f6d51e064d9c0da1ee5
raysonoon <raysonoonhere@gmail.com>
2024-06-30 14:44:47 +0800

    Complete object exercises using array methods

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheAge = (birth, death) => {
+    // If person is not dead i.e. year of death undefined
+    if (!death) {
+        console.log(death);
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        let oldestAge = findTheAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = findTheAge(current.yearOfBirth, current.yearOfDeath);
+        // Return people object
+        return oldestAge < currentAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
